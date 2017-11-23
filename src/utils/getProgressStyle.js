509b5135323216d0e89b92be02c3d75ad8033554
Rostyslav <rostyslav.rozhko@gmail.com>
2017-11-23 20:25:51 +0200

    fix minion right pos

--- src/utils/getProgressStyle.js
+++ src/utils/getProgressStyle.js
@@ -9,11 +9,11 @@ export const getProgressStyle = (perc, fields) => {
   switch (true) {
     case perc > 150: return {icon: PilotIcon, style: {backgroundColor: '#94e100'}}
     case perc > 125 && perc < 151: return {icon: CapitainIcon, style: {backgroundColor: '#94e100'}}
-    case perc >= 100 && perc < 126: return {icon: DriverIcon, style: {backgroundColor: '#ffda44'}}
+    case perc >= 100 && perc < 126: return {icon: DriverIcon, style: {backgroundColor: '#94e100'}}
     case perc > 40 && perc < 100: return {icon: Bussinessmanicon, style: {backgroundColor: '#ffda44'}}
     case perc > 30 && perc < 71: return {icon: StudentIcon, style: {backgroundColor: '#ff8400', color: 'white'}}
-    case perc >= 6  && perc < 31: return {icon: SchoolboyIcon, style: {backgroundColor: '#ff5400', color: 'white', right: fields === 2 ? '-15px' : '-30px'}}
-    case perc >= 0  && perc < 6: return {icon: SchoolboyIcon, style: {backgroundColor: '#ff5400', color: 'white', right: '-45px'}}
+    case perc >= 6  && perc < 31: return {icon: SchoolboyIcon, style: {backgroundColor: '#ff5400', color: 'white', right: '0px'}}
+    case perc >= 0  && perc < 6: return {icon: SchoolboyIcon, style: {backgroundColor: '#ff5400', color: 'white', right: fields === 2 ? '-20px' : '-30px'}}
     default: return {icon: PilotIcon, style: {backgroundColor: '#94e100'}}
   }
 }
